3482c5768fdf1f736140b2e5d0069bf60deec551
Augusto Cesar <augustocesar.udi@gmail.com>
2021-05-21 01:14:45 -0400

    Project created

---
+++ backend/src/main/java/com/devsuperior/crudcap01/Crudcap01Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.crudcap01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crudcap01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crudcap01Application.class, args);
+	}
+
+}
